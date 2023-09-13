a5ad233fa7f5451f0dbe120ef4ab91a5e365431c
Bernd Janzen <103428887+manjou@users.noreply.github.com>
2023-09-13 09:48:26 +0200

    added pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-alert('Hello world!');
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList = [
+  {index: 1, name: "Bulbasaur", type: ["GRASS", "POISON"], total: 318}, 
+  {index: 2, name: "Ivysaur", type: ["GRASS", "POISON"], total: 405}, 
+  {index: 3, name:"Venusaur", type: ["GRASS", "POISON"], total: 525}, 
+  {index: 4, name: "Charmander", type: ["GRASS", "POISON", total: 625]}
+];
