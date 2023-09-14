df94a226aed6c0d0595bd1246c771cf7f2c65dd2
Bernd Janzen <103428887+manjou@users.noreply.github.com>
2023-09-14 13:15:39 +0200

    basic styling, js array and loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
+// Used to store the pokemonList array
+
 let pokemonList = [];
 pokemonList = [
-  {index: 1, name: "Bulbasaur", type: ["GRASS", "POISON"], total: 318}, 
-  {index: 2, name: "Ivysaur", type: ["GRASS", "POISON"], total: 405}, 
-  {index: 3, name:"Venusaur", type: ["GRASS", "POISON"], total: 525}, 
-  {index: 4, name: "Charmander", type: ["GRASS", "POISON", total: 625]}
+  {index: 1, name: "Bulbasaur", type: ["GRASS", "POISON"], height: 0.7, total: 318}, 
+  {index: 2, name: "Ivysaur", type: ["GRASS", "POISON"], height: 1, total: 405}, 
+  {index: 3, name:"Venusaur", type: ["GRASS", "POISON"], height: 2, total: 525}, 
+  {index: 4, name: "Charmander", type: ["GRASS", "POISON"], height: 0.6, total: 625}
 ];
+
+// for loop to iterate over the pokemonList array and print the name and height of each pokemon. 
+// it also checks if the pokemon is over 1m.
+
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 1) {
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!" + "<br>");
+  }else {
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "<br>");
+  }
+}
+
+
