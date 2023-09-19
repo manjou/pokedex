8ce40c40e1fb74450f3e2bf08965deadc73810f6
Bernd Janzen <103428887+manjou@users.noreply.github.com>
2023-09-19 08:41:35 +0200

    for loop to print array details

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Used to store the pokemonList array
 
-const pokemonList = [];
+let pokemonList = [];
 pokemonList = [
   {index: 1, name: "Bulbasaur", type: ["GRASS", "POISON"], height: 0.7, total: 318}, 
   {index: 2, name: "Ivysaur", type: ["GRASS", "POISON"], height: 1, total: 405}, 
@@ -12,12 +12,25 @@ pokemonList = [
 // it also checks if the pokemon is over 1m.
 
 
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 1) {
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!" + "<br>");
-  }else {
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "<br>");
+// for (let i = 0; i < pokemonList.length; i++) {
+//   if (pokemonList[i].height > 1) {
+//     document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!" + "<br>");
+//   }else {
+//     document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "<br>");
+//   }
+// }
+
+
+// declaration of function printArrayDetails
+function printArrayDetails(list) {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].height > 1) {
+      document.write(list[i].name + " (height: " + list[i].height + ") - Wow, that's big!" + "<br>");
+    }else {
+      document.write(list[i].name + " (height: " + list[i].height + ")" + "<br>");
+    }
   }
 }
+
 
 
