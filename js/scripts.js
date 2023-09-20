3cbfc42621d47bbdd38636d43fb9d1be9c07c6ce
Bernd Janzen <103428887+manjou@users.noreply.github.com>
2023-09-20 14:43:58 +0200

    ex.1.5 part 2 + bonus task. IIFE added plus functions add, addv, getAll and FindByName, forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,53 @@
-// Create pokemonList array
+// IIFE to create a pokemonRepository variable that is not global and can be accessed publicly
+// with functions add, addv, getAll, and findByName
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name: "Bulbasaur", type: ["GRASS", "POISON"], height: 0.7, total: 318}, 
+    {name: "Ivysaur", type: ["GRASS", "POISON"], height: 1, total: 405}, 
+    {name:"Venusaur", type: ["GRASS", "POISON"], height: 2, total: 525}, 
+    {name: "Charmander", type: ["GRASS", "POISON"], height: 0.6, total: 625}
+  ];
+  
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
 
-let pokemonList = [
-  {index: 1, name: "Bulbasaur", type: ["GRASS", "POISON"], height: 0.7, total: 318}, 
-  {index: 2, name: "Ivysaur", type: ["GRASS", "POISON"], height: 1, total: 405}, 
-  {index: 3, name:"Venusaur", type: ["GRASS", "POISON"], height: 2, total: 525}, 
-  {index: 4, name: "Charmander", type: ["GRASS", "POISON"], height: 0.6, total: 625}
-];
+    function addv(pokemon) {
+      if (typeof pokemon === "object" && pokemon !== null) {
+        const keys = Object.keys(pokemon);
+        const requiredKeys = ["name", "type", "height", "total"];
+        if (keys.length === requiredKeys.length && requiredKeys.every(key => keys.includes(key))) {
+        pokemonList.push(pokemon);
+      } else {
+        console.log("Error: invalid data type for Pokemon object.");
+      }
+    }
 
+    function getAll() {
+      return pokemonList;
+    }
+
+    function findByName(name) {
+      let query = document.getElementById("searchBar").value.toLowerCase();
+      let matchingPokemon = pokemonList.filter(function(pokemon) {
+        return pokemon.name.toLowerCase() === name.toLowerCase();
+      )};
+      return matchingPokemon;
+    }
+
+
+    return {
+      add: add,
+      addv: addv,
+      getAll: getAll,
+      findByName: findByName
+    };
+  
+}());
+// End of IIFE pokemonRepository
+
+// 
+let matchingPokemon = pokemonRepository.findByName("getElementById");
 
 // declaration of function printArrayDetails
 function printArrayDetails(pokemon) {
@@ -17,7 +58,7 @@ function printArrayDetails(pokemon) {
     }
   }
   // forEach loop to print out the pokemonList array
-  pokemonList.forEach(printArrayDetails);
+  pokemonRepository.getAll().forEach(printArrayDetails);
 
 
 
